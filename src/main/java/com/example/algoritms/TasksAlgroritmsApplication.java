cdadbcc33f0e787e07e63ee1a79326bda2552f90
Murad Raimjanov <raimzhanov.muradil@gmail.com>
2024-09-06 14:19:36 +0300

    first commit

---
+++ src/main/java/com/example/algoritms/TasksAlgroritmsApplication.java
@@ -0,0 +1,13 @@
+package com.example.algoritms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TasksAlgroritmsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TasksAlgroritmsApplication.class, args);
+    }
+
+}
